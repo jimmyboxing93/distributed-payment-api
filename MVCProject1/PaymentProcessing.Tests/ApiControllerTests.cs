b22d5d75343a8993cb660a410d08890c58773ebb
Jimmy <jimmyboxing93@gmail.com>
2026-03-13 01:25:50 -0400

    test: implement happy path for payment and rename tests for clarity

--- MVCProject1/PaymentProcessing.Tests/ApiControllerTests.cs
+++ MVCProject1/PaymentProcessing.Tests/ApiControllerTests.cs
@@ -10,8 +10,9 @@ namespace PaymentProcessing.Tests
 {
 	public class ApiControllerTests
 	{
+		// Testing null credit cards via post method. 
 		[Fact]
-		public void Test1()
+		public void Payment_ReturnBadRequest_WhenCreditCardIsEmpty()
 		{
 			var mockService = new Mock<IUserInfo>();
 			var mockLogger  = new Mock<ILogger<ApiController>>();
@@ -28,6 +29,28 @@ namespace PaymentProcessing.Tests
 
 			// Used to make sure the DB is never called. 
 			mockService.Verify(s => s.AddCreditCard(It.IsAny<UserInfo>()), Times.Never);
+
+		}
+		// Testing happy path for post method
+		[Fact]
+		public void Payment_ReturnsOk_WhenDataIsValid() 
+		{
+			var mockService = new Mock<IUserInfo>();
+			var mockLogger = new Mock<ILogger<ApiController>>();
+			var controller = new ApiController(mockService.Object, mockLogger.Object);
+
+			// Guard clause
+			var validModel = new UserInfo
+			{
+				UserID = Guid.NewGuid(),
+				creditCardNumber = "1234567890"
+			};
+
+			var result = controller.Payment(validModel);
+
+			Assert.IsType<OkObjectResult>(result);
+
+			mockService.Verify(s => s.AddCreditCard(It.IsAny<UserInfo>()), Times.Once);
 
 		}
 	}
